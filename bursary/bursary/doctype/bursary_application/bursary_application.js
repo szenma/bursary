d36fb21b208fe87b0d3131be4791c32dcc55e0a7
root <root@server1.tsea.app>
2023-05-12 11:12:01 +0000

    Updated app files

---
+++ bursary/bursary/doctype/bursary_application/bursary_application.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, zenma and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bursary Application', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ bursary/bursary/doctype/bursary_application/test_bursary_application.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, zenma and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBursaryApplication(FrappeTestCase):
+	pass
